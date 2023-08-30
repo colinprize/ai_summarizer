cfa08983637f2bcc7f2d7a22e03ca6b9ce6a82b4
colinprize <109234316+colinprize@users.noreply.github.com>
2023-08-29 22:04:22 -0700

    api call works

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,11 +1,34 @@
 import { useState, useEffect } from 'react';
 import { copy, linkIcon, loader, tick } from '../assets';
+import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  });
+
+  const [ getSummary, { error, isFetching}] = useLazyGetSummaryQuery();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const {data} = await getSummary({articleUrl: article.url });
+
+    if(data?.summary) {
+      const newArticle = { ...article, summary: data.summary}
+
+      setArticle(newArticle);
+
+      console.log(newArticle);
+    }
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form className= "relative flex justify-center items-center"
+        onSubmit={handleSubmit}
         >
           <img
           src={linkIcon}
@@ -15,14 +38,14 @@ const Demo = () => {
           <input
             type="url"
             placeholder='Enter a URL'
-            value = ""
-            onChange={() => {}}
+            value = {article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value})}
             required
             className="url_input peer"
           />
           <button
             type="submit"
-            className="submit_btn peer peer-focus:border-gray-700 peer-focus:text-gray-700"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
             ></button>
         </form>
       </div>
